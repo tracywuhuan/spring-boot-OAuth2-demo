a496925120a51c5a61286c9f6556c14977efb3be
wuhuan <wuhuan@macbook-pro-2.cn.ibm.com>
2019-05-29 16:42:29 +0800

    Initialize project

---
+++ src/main/java/com/example/demo/SpringBootOAuth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOAuth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOAuth2DemoApplication.class, args);
+	}
+
+}
